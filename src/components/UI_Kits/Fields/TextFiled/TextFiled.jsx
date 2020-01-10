6f2c5e267df5abc36cd16811f980da637915bcab
tregdor <49729985+tregdor@users.noreply.github.com>
2020-01-11 00:39:20 +0300

    Добавил Text Filed

--- src/components/UI_Kits/Fields/TextFiled/TextFiled.jsx
+++ src/components/UI_Kits/Fields/TextFiled/TextFiled.jsx
@@ -1,23 +1,45 @@
-import React from "react";
+import React, {useState} from "react";
 import injectSheet from 'react-jss';
-import {DarkShade25} from "../../../../Colors";
+import {DarkShade25, DarkShade50} from "../../../../Colors";
 import {Body} from "../../../../Fonts";
 
 const classes = {
-  input:{
-    width:320,
-    height:44,
-    padding:'0 0 0 15px',
+  input: {
+    width: 320,
+    height: 44,
+    padding: '0 0 0 15px',
     background: '#FFFFFF',
-    border:`1px solid ${DarkShade25}`,
+    border: props => props.isActive ? `1px solid ${ DarkShade50 }` : `1px solid ${ DarkShade25 }`,
     boxSizing: 'border-box',
     borderRadius: 4,
+    outline: 'none',
     ...Body,
+    color: props => props.isActive ? DarkShade50 : DarkShade25,
   }
 };
 
-const TextField = ({placeholder = 'Email',classes,...rest}) => {
-  return <input type='text' placeholder={placeholder} className={classes.input}/>
+const TextField = ({placeholder = 'Email', ...rest}) => {
+  const [isFocus, setIsFocus] = useState(false);
+  const [isMouseOver, setMouseOver] = useState(false);
+  const mouseOver = (state) => {
+    if (isFocus) {
+      if (state) setIsFocus(state)
+    } else setMouseOver(state);
+  };
+
+  const focus = (state) => {
+    setIsFocus(state);
+  };
+
+  return <StylesFiled mouseOver={ mouseOver } focus={ focus }
+                      isActive={ isFocus || isMouseOver } placeholder={ placeholder }/>
 };
 
-export default injectSheet(classes)(TextField);
+const StylesFiled = injectSheet(classes)(({classes, placeholder, mouseOver, focus, ...rest}) => {
+
+  return <input type='text' placeholder={ placeholder } className={ classes.input }
+                onMouseOver={ () => mouseOver(true) } onMouseLeave={ () => mouseOver(false) }
+                onFocus={ () => focus(true) } onBlur={ () => focus(true) }/>
+});
+
+export default TextField;
