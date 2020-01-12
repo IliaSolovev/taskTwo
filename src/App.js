01467d98eaa4c7781837bc559c3666bb6ab9b48e
tregdor <49729985+tregdor@users.noreply.github.com>
2020-01-12 23:36:04 +0300

    Добавил Paginator

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import React from 'react';
 import './App.css';
 import './components/UI_Kits/Buttons/CheckboxButton/checkbox.css'
-import BulletList from "./components/UI_Kits/BulletList/BulletList";
-import RoomDetails from "./components/UI_Kits/RoomDetails/RoomDetails";
-import LikeButton from "./components/UI_Kits/Buttons/LikeButton/LikeButton";
-import ProductReview from "./components/UI_Kits/ProductReview/ProductReview";
+import Paginator from './components/UI_Kits/Paginator/Paginator';
 
 function App (){
   return (
     <div className="App">
+      <Paginator/>
     </div>
   );
 }
