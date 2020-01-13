cdb70b6de3a1bad2e468955b9b0044018d421be8
tregdor <49729985+tregdor@users.noreply.github.com>
2020-01-13 19:01:21 +0300

    Добавил Range Slider

--- src/App.js
+++ src/App.js
@@ -2,11 +2,12 @@ import React from 'react';
 import './App.css';
 import './components/UI_Kits/Buttons/CheckboxButton/checkbox.css'
 import SubscriptionTextField from "./components/UI_Kits/Fields/SubscriptionTextField/SubscriptionTextField";
+import RangeSlider from "./components/UI_Kits/RangeSlider/RangeSlider";
 
 function App (){
   return (
     <div className="App">
-      <SubscriptionTextField/>
+      <RangeSlider/>
     </div>
   );
 }
